9bc5756fd10272365ced1236c30514b8b696d7b9
guabutian <2351180282@qq.com>
2018-08-29 01:18:43 +0800

    提交一个测试

--- src/test/Solution1Test.cpp
+++ src/test/Solution1Test.cpp
@@ -24,7 +24,12 @@ namespace {
         Base::Solution1 a;
         a.test1();
 	}
-
+    TEST(Base_Solution1_test1_Test,test2)
+    {
+        //测试数据：
+        Base::Solution1 a;
+        std::cout<<a.LongestWord("abcdefg");
+    }
 
 
 }
